30f61d3362251a31fab6ba5c15da12978bcd16b7
Berkay Güneş <gunessberkay@gmail.com>
2023-12-06 18:42:02 +0300

    flutterProject_mealApp1
    flutterProject_mealsApp1

---
+++ lib/screens/categories.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:mealsapp/data/datas.dart';
+import 'package:mealsapp/models/category.dart';
+import 'package:mealsapp/screens/meal_list.dart';
+import 'package:mealsapp/widgets/category_card.dart';
+
+class Categories extends StatelessWidget {
+  const Categories({Key? key}) : super(key: key);
+
+  // Stful widgetlarda => context globaldir
+  // Stless widgetlarda => Sadece build fonksiyonunda context'e erişilebilir.
+  void _selectCategory(BuildContext context, Category category) {
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (ctx) => MealList(
+            meals: meals
+                .where((element) => element.categoryId == category.id)
+                .toList())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Bir kategori seçin")),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 243, 142, 33),
+              ),
+              child: Text('Bir kategori seçin',),
+            ),
+            for (final category in categories)
+            ListTile(
+              title: Text(category.name),
+              onTap: () {
+                _selectCategory(context, category);
+              },
+            )
+          ],
+        ),
+      ),
+
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+          childAspectRatio: 2,
+        ),
+        children: [
+          for (final category in categories)
+            CategoryCard(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
+        ],
+      ),
+    );
+  }
+}
